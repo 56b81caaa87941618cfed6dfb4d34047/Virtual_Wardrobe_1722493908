761c45d5b387fb67f92fe8cf78c2fd2fa48a3025
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-01 06:35:53 +0000

    Code generation

--- components/simple_header_component_1722493909.js
+++ components/simple_header_component_1722493909.js
@@ -3,15 +3,15 @@
 
 Vue.component("simple_header_component_1722493909", {
     template: `
-    <header id="header-section" style="min-height: 30px"  class="w-full">
-        <nav id="nav-bar" class="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
+    <header id="header-section" style="min-height: 30px" class="w-full">
+        <nav id="nav-bar" class="bg-opacity-20 bg-pink-300 backdrop-filter backdrop-blur-lg border border-pink-200 rounded-lg shadow-lg py-2.5">
             <div id="nav-container" class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
                 <a id="logo-link" href="#" class="flex items-center">
-                    <img id="logo-image" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                    <span id="app-name" class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">FabricFinds - Your Stylish Destination</span>
+                    <img id="logo-image" src="./images/logo.svg" class="h-8 mr-3 sm:h-10" alt="Landwind Logo" />
+                    <span id="app-name" class="self-center text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-500">FabricFinds - Your Stylish Destination</span>
                 </a>
                 <div id="menu-toggle-button" class="flex items-center lg:order-2">
-                    <button id="collapse-button" data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                    <button id="collapse-button" data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-purple-600 rounded-lg lg:hidden hover:bg-pink-100 focus:outline-none focus:ring-2 focus:ring-pink-300" aria-controls="mobile-menu-2" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
                         <svg id="menu-open-icon" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
@@ -23,10 +23,10 @@ Vue.component("simple_header_component_1722493909", {
                 </div>
                 <div id="mobile-menu-2" class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
                     <ul id="nav-list" class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                        <li id="nav-item-home"><a href="#" class="block py-2 pl-3 pr-4 text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700 lg:p-0 dark:text-white" aria-current="page">Curated Collections</a></li>
-                        <li id="nav-item-1"><a href="#" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Personalized Styling</a></li>
-                        <li id="nav-item-2"><a href="#" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Exclusive Deals</a></li>
-                        <li id="nav-item-3"><a href="#" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">About Us</a></li>
+                        <li id="nav-item-home"><a href="#" class="block py-2 pl-3 pr-4 text-purple-700 bg-pink-100 rounded-lg lg:bg-transparent lg:text-purple-600 lg:p-0 lg:hover:text-pink-500 transition-colors duration-300" aria-current="page">Curated Collections</a></li>
+                        <li id="nav-item-1"><a href="#" class="block py-2 pl-3 pr-4 text-purple-600 rounded-lg hover:bg-pink-100 lg:hover:bg-transparent lg:hover:text-pink-500 lg:p-0 transition-colors duration-300">Personalized Styling</a></li>
+                        <li id="nav-item-2"><a href="#" class="block py-2 pl-3 pr-4 text-purple-600 rounded-lg hover:bg-pink-100 lg:hover:bg-transparent lg:hover:text-pink-500 lg:p-0 transition-colors duration-300">Exclusive Deals</a></li>
+                        <li id="nav-item-3"><a href="#" class="block py-2 pl-3 pr-4 text-purple-600 rounded-lg hover:bg-pink-100 lg:hover:bg-transparent lg:hover:text-pink-500 lg:p-0 transition-colors duration-300">About Us</a></li>
                     </ul>
                 </div>
             </div>
