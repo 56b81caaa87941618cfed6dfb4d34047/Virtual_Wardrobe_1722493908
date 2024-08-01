8066e7b628de3228c1ce56db77fb64f5efc5cc36
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-01 06:36:53 +0000

    Merge code from frontend

--- components/simple_hero_component_1722493909.js
+++ components/simple_hero_component_1722493909.js
@@ -4,7 +4,7 @@
 */
 Vue.component("simple_hero_component_1722493909", {
     template: `
-        <section id="hero-section" class="bg-gradient-to-r from-pink-500 to-purple-600 flex-1">
+        <section id="hero-section" style="min-height: 910px" class="bg-gradient-to-r from-pink-500 to-purple-600 flex-1">
             <div id="hero-container" class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
                 <div id="hero-text-container" class="mr-auto place-self-center lg:col-span-7 bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg rounded-xl p-8 shadow-lg">
                     
@@ -28,7 +28,7 @@ Vue.component("simple_hero_component_1722493909", {
                     
                 </div>
                 <div id="hero-image-container" class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                    <img id="hero-image" src="./images/hero.png" alt="hero image" class="rounded-xl shadow-lg">
+                    <img id="hero-image" src="https://makeinfinite-mentat-test.azurewebsites.net/get_image/tmppv719vdl/hero.png" alt="hero image" class="rounded-xl shadow-lg">
                 </div>
             </div>
         </section>            
